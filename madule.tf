7a01fde01ebe7a66bc24c4a464d861d1d2e0bf9a
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-04 22:32:08 +0530

    Create madule.tf

---
+++ madule.tf
@@ -0,0 +1,45 @@
+variable "profile_name" {
+    type = string
+    default = "default"
+}
+
+variable "region_name" {
+    type = string
+    default = "us-east-2"
+}
+
+variable "key_name_name" {
+    type = string
+    default = " "
+}
+
+variable "instance_id_name" {
+    type = string
+    default = "i-03f429574f46d2e9f "
+}
+
+variable "instance_type_name" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "az_name" {
+    type = string
+    default = "us-east-2b"
+}
+
+variable "public_key_name" {
+    type = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL/qQdAC7N5GH+dy2FSDJcyCjNBIZ5yV6WkjvnPoLDGGHnwbmBlIu6aPEP/pAtnd0aM7olEmlHXDFSeAxIl6hcrjoF2IpNsrDBOoNgBg+TRdzahzIeDCFgDxwbdt7SmtEAy6t3g6jBd59J5uBbdiFHz8tGt2SttRg58ioM5NhtAAOTGgxXwjIDRXBsHjywbNj1JYbJeg+R9TWjRHqDUqlRhAPsARHVmqzvWNPlkIa+tTBGEzk8B49ScVj5B1sjZg5FLibdiOvTU2rBvW+FdEMGkFWt2UoJn0xJqUfVy8ShqIrDfyUB5EkxR+fvlKTKRDF2KUDxlsz1CSp8/GvDXAnh Ec2all "
+}
+
+variable "iam_instance_profile_name" {
+    type = string
+    default = "ami-0fb653ca2d3203ac1 "
+}
+
+
+variable "name_name" {
+    type = string
+    default = "deva_1 "
+}
